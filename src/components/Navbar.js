b7b55c758a34585926f574d3690a8df3d2990eba
AdityNil <sirf_aadi@outlook.in>
2024-06-12 14:38:27 +0530

    navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,22 +1,54 @@
-import React from 'react'
-//import { useNavigate } from 'react-router-dom'
+import React from 'react';
+//import { useNavigate } from 'react-router-dom';
 
-const navbar = () => {
-
+const Navbar = () => {
   return (
-    <div>
-        <div>
-            <h2>Artisan<spam>Alley</spam></h2>
-        </div>
-        <div>
-            <h3>Explore</h3>
-            <h3>About</h3>
-            <h3>Business</h3>
-            <h3>Log in</h3>
-            <h3>Sign Up</h3>
-        </div>      
+    <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center',fontFamily:'San Francisco, Helvetica, Arial, san-serifery',}}>
+      <div>
+        <h3 style={styles.heading}>Artisan <span style={{color:'red'}}>Alley</span></h3>
+      </div>
+      <div style={styles.navLinks}>
+        <h4 style={styles.navLink}>Explore</h4>
+        <h4 style={styles.navLink}>About</h4>
+        <h4 style={styles.navLink}>Business</h4>
+        <h4 style={styles.navItem1}>Log in</h4>
+        <h4 style={styles.navItem2}>Sign Up</h4>
+      </div>      
     </div>
-  )
-}
+  );
+};
 
-export default navbar
+const styles = {
+  heading: {
+    fontFamily:'Snell Roundhand, cursive',
+    fontSize: '5vh',
+    paddingLeft:'5vh',
+    
+  },
+  navLinks: {
+    fontSize: '2.5vh',
+    display: 'flex',
+    gap: '7vh',
+    marginRight:'2vh',
+    cursor: 'pointer', 
+  },
+  navLink:{
+    paddingTop:'1vh'
+
+  },
+  navItem1: {
+    backgroundColor:"#e60023",
+    color:'white',
+    borderRadius:'10vh',
+    padding: '1vh 1.2vh',
+    
+  },
+  navItem2: {
+    backgroundColor:"#e9e9e9",
+    color:'black',
+    borderRadius:'10vh',
+    padding: '1vh 1.2vh',
+  }
+};
+
+export default Navbar;
